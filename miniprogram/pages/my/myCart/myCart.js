5c57062338ba7c18d49065147dc9265680ffa872
JingYi Liu <271182779@qq.com>
2020-10-18 23:57:07 +0800

    update my

---
+++ miniprogram/pages/my/myCart/myCart.js
@@ -0,0 +1,91 @@
+// miniprogram/pages/my/myCart/myCart.js
+Page({
+  data: {
+    carts:[],               // 购物车列表
+    hasList:false,          // 列表是否有数据
+    totalPrice:0,           // 总价，初始为0
+    selectAllStatus:true,    // 全选状态，默认全选
+    obj:{
+        name:"hello"
+    }
+  },
+  onShow() {
+    this.setData({
+      hasList: true,
+      carts:[
+        {id:1,title:'耳机',image:'/images/goods/02.jpg',num:1,price:360,selected:true},
+        {id:2,title:'卸妆水',image:'/images/goods/03.jpg',num:1,price:20,selected:true}
+      ]
+    });
+    this.getTotalPrice();
+  },
+  /**
+   * 当前商品选中事件
+   */
+  selectList(e) {
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    const selected = carts[index].selected;
+    carts[index].selected = !selected;
+    this.setData({
+      carts: carts
+    });
+    this.getTotalPrice();
+  },
+
+  /**
+   * 删除购物车当前商品
+   */
+  deleteList(e) {
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    carts.splice(index,1);
+    this.setData({
+      carts: carts
+    });
+    if(!carts.length){
+      this.setData({
+        hasList: false
+      });
+    }else{
+      this.getTotalPrice();
+    }
+  },
+
+  /**
+   * 购物车全选事件
+   */
+  selectAll(e) {
+    let selectAllStatus = this.data.selectAllStatus;
+    selectAllStatus = !selectAllStatus;
+    let carts = this.data.carts;
+
+    for (let i = 0; i < carts.length; i++) {
+      carts[i].selected = selectAllStatus;
+    }
+    this.setData({
+      selectAllStatus: selectAllStatus,
+      carts: carts
+    });
+    this.getTotalPrice();
+  },
+
+  
+  /**
+   * 计算总价
+   */
+  getTotalPrice() {
+    let carts = this.data.carts;                  // 获取购物车列表
+    let total = 0;
+    for(let i = 0; i<carts.length; i++) {         // 循环列表得到每个数据
+      if(carts[i].selected) {                     // 判断选中才会计算价格
+        total +=  carts[i].price;   // 所有价格加起来
+      }
+    }
+    this.setData({                                // 最后赋值到data中渲染到页面
+      carts: carts,
+      totalPrice: total.toFixed(2)
+    });
+  }
+
+})
