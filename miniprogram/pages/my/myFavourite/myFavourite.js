383ffcf17c5bf5b0798811e13b8ef79689cb5f93
JingYi Liu <271182779@qq.com>
2020-11-20 22:29:42 +0800

    finish first version of favorite

--- miniprogram/pages/my/myFavourite/myFavourite.js
+++ miniprogram/pages/my/myFavourite/myFavourite.js
@@ -7,25 +7,14 @@ Page({
   data: {
     goodsList: [
   ],
+  hasList:true,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this
-    wx.cloud.callFunction({
-      name: 'getFavoritesByUserId',
-      data:{
-      },
-      success(res) {
-        console.log(res)
-        console.log('成功', res.result.data.list);
-        that.setData({
-          goodsList: res.result.data.list,
-        });
-      },
-    })
+
   },
 
   /**
@@ -39,7 +28,26 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var that = this
+    wx.cloud.callFunction({
+      name: 'getFavoritesByUserId',
+      data:{
+      },
+      success(res) {
+        console.log('成功', res.result.data);
+        that.setData({
+          goodsList: res.result.data,
+        });
+        if(res.result.data)
+        {
+          if(res.result.data.length){
+          that.setData({
+            hasList: true,
+          });
+        }
+        }
+      },
+    })
   },
 
   /**
@@ -75,5 +83,34 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+
+  deleteFavorite(e) {
+      const index = e.currentTarget.dataset.index;
+      let goodsList = this.data.goodsList;
+      //调用云函数，在后端也把记录删掉
+      wx.cloud.callFunction({
+        name: 'delFavorite',
+        data:{
+          goodId:goodsList[index]._id
+        },
+        success(res) {
+          console.log('成功');
+        },
+      })
+      goodsList.splice(index,1);
+      this.setData({
+        goodsList : goodsList 
+      });
+  /*
+      if(this.data.goodsList.length==0)
+      {
+        this.setData({
+          hasList : false
+        });
+      }
+      */
+  },
+
 })
