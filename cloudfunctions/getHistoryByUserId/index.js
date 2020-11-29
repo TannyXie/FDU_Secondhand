1eef323bcafb91a9754a5bdbffe02b9caaaae8e9
Silhouette2 <lyhhh37@outlook.com>
2020-11-29 23:36:58 +0800

    Changed the structure of class user

--- cloudfunctions/getHistoryByUserId/index.js
+++ cloudfunctions/getHistoryByUserId/index.js
@@ -16,11 +16,22 @@ const _ = db.command
 const time = require('./util.js')
 
 exports.main = async (event, context) => {
-  const userId = event.userId ? event.userId : cloud.getWXContext().OPENID
+  const openid = cloud.getWXContext().OPENID
+  var userId = event.userId;
   if (userId == null) {
-    return {
-      statusCode: 400,
-      statusMsg: 'can not get userid'
+    try {
+      userResult = await db.collection('user').where({
+        openid: _.eq(openid)
+      }).get()
+      console.log(userResult)
+      userId = userResult.data[0]._id
+      if (userId == null) throw 'openid may not exist'
+    } catch (err) {
+      console.log(err)
+      return {
+        statusCode: 500,
+        statusMsg: 'can not get userid'
+      }
     }
   }
 
