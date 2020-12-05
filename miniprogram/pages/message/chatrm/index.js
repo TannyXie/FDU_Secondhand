7198ec17ea36e8da2a1608296ae7785b27396015
TannyXie <tannyxie@TannyXiedeMacBook.local>
2020-12-05 16:37:29 +0800

    Get sellerId from userpage and complete dialogue logic. UI pending fix

--- miniprogram/pages/message/chatrm/index.js
+++ miniprogram/pages/message/chatrm/index.js
@@ -8,21 +8,77 @@ Page({
     i:0,
     inputMessage : '',
     histMess : [
-    ]
+    ],
+    Mess : [],
+    timer : '',
+    messages: '',
+    speakee: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
+    this.setData({
+      speakee: options.sellerId
+    })
+  }, 
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    var that = this;
+    that.data.setInter = setInterval(
+      function(){
+        console.log('here');
+        var result = '';
+        wx.cloud.callFunction({
+          name: 'getMessages',
+          data: {
+            thisUserId: 'fakeuser1',
+            anotherUserId: 'fakeuser2'
+          },
+          success(res) {
+            console.log(res);
+            if(res.result.statusMsg=='wrong code')
+            {
+              wx.showModal({
+                title: '消息提示',
+                content: '添加消息失败',
+              })
+            }
+            else{
+              that.setData({
+                messages: res.result.data
+              })
+            }
+          },
+        })
+      },5000
+    )
+  },
 
+  renewMess(e){
+    wx.cloud.callFunction({
+      name: 'getMessages',
+      data: {
+        receiver: '',
+        content: this.data.inputMessage
+      },
+      success(res) {
+        console.log(res);
+        if(res.result.statusMsg=='wrong code')
+        {
+          wx.showModal({
+            title: '消息提示',
+            content: '添加消息失败',
+          })
+        }
+        else{
+        }
+      },
+    })
   },
 
   /**
@@ -81,7 +137,6 @@ Page({
     this.setData({
       inputMessage : e.detail.value,
     });
-    console.log(this.data.inputMessage);
   },
 
   inputRenew(e){
@@ -91,11 +146,36 @@ Page({
       response: 'this is a response'
     });
     var i = this.data.i;
+    console.log('the inputMessage is '+this.data.inputMessage);
+    wx.cloud.callFunction({
+      name: 'addMessage',
+      data: {
+        senderId: 'fakeuser1',
+        receiverId: 'fakeuser2',
+        content: this.data.inputMessage
+      },
+      success(res) {
+        console.log(res);
+        if(res.result.statusMsg=='wrong code')
+        {
+          wx.showModal({
+            title: '消息提示', 
+            content: '添加消息失败',
+          })
+        }
+      },
+    })
     this.setData({
       inputMessage : '',
       i : i+1,
       histMess : history
-    })
-
-  }
+    });
+  },
 })
+/*
+
+    <view class="input-box2">
+      {{item.content}}
+    </view>
+
+*/
