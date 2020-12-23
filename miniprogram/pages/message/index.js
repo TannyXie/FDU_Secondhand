53ec40f8d34e595a36c7da3ab9da16997634db81
QiaowenYoung <17307130294@fudan.edu.cn>
2020-12-23 18:05:08 +0800

    flush message

--- miniprogram/pages/message/index.js
+++ miniprogram/pages/message/index.js
@@ -25,7 +25,7 @@ Page({
   /**
    * 生命周期函数 —— 监听页面加载
    */
-  onLoad: function (options) {
+  onShow: function (options) {
     
     var that = this;
 
@@ -133,8 +133,8 @@ Page({
     });
     console.log(this.data.messageList);
   },
-  onShow(){
-    var that = this;
+  onLoad(){
+    
   },
   goToBangDan: function() {
     wx.navigateTo({
